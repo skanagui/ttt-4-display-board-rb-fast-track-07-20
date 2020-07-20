cae5cf439e42968543e4839c6c7c5644d5816808
confident-architect-8379 <s.kanagui@gmail.com>
2020-07-20 19:24:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board = [(" "[0], " "[1], " "[2], " "[3], "X"[4], " "[5], " "[6], " "[7], " "[8])]
 
-def display_board(board)
+def display_board
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
