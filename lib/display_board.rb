fdb3ad945c0c30579ded703fe8db9e9fffff2472
confident-architect-8379 <s.kanagui@gmail.com>
2020-07-20 19:23:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
+
+board = [(" "[0], " "[1], " "[2], " "[3], "X"[4], " "[5], " "[6], " "[7], " "[8])]
+
+def display_board(board)
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
